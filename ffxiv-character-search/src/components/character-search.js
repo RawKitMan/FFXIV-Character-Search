f962f9bcb99b00f05ff91089bce306fe8fff7e7e
CJ Vaughn <cj.vaughn052284@gmail.com>
2022-03-16 09:14:47 -0700

    setup plus initial test. will write more tests tomorrow

---
+++ ffxiv-character-search/src/components/character-search.js
@@ -0,0 +1,5 @@
+export class CharacterSearch {
+    //TODO: render the app on the created hook
+    //Make a spot where everything will go
+    //Add a box for search parameters. Use a form here.
+}
---
+++ ffxiv-character-search/test/unit/character-search.spec.js
@@ -0,0 +1,30 @@
+import { StageComponent } from 'aurelia-testing';
+import { bootstrap } from 'aurelia-bootstrapper';
+import { PLATFORM } from 'aurelia-pal';
+import { findAll } from '../../node_modules/domutils/lib/querying';
+
+describe('CharacterSearch', () => {
+    let component;
+
+    beforeEach(() => {
+        component = StageComponent
+            .withResources(PLATFORM.moduleName('../../src/components/character-search'))
+            .inView('<character-search></character-search>');
+    });
+
+    it('should render headerElement', done => {
+        component.create(bootstrap).then(() => {
+            console.log('component', component);
+            const headerElement = document.querySelector('#header');
+            expect(headerElement.innerHTML).toBe('FFXIV Character Search');
+            done();
+        })
+    });
+
+    afterEach(() => {
+        if (component) {
+            component.dispose();
+            component = null;
+        }
+    })
+});
