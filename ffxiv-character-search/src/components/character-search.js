83bc80171ffc70d005e4c79df1faa1427ac06afd
CJ Vaughn <cj.vaughn052284@gmail.com>
2022-03-17 11:06:01 -0700

    new component added. Figured out data binding and proper referencing during api responses.

--- ffxiv-character-search/src/components/character-search.js
+++ ffxiv-character-search/src/components/character-search.js
@@ -1,5 +1,34 @@
 export class CharacterSearch {
-    //TODO: render the app on the created hook
-    //Make a spot where everything will go
-    //Add a box for search parameters. Use a form here.
+    imageSrc = '';
+    characterJob = ''
+    get showImg() {
+        return this.imageSrc !== '';
+    }
+
+    created() {
+        this.getCharacterInfo();
+    }
+
+    getCharacterInfoById(id) {
+        let xhttp = new XMLHttpRequest();
+        const instance = this;
+        xhttp.open('GET', `https://xivapi.com/character/${id}`);
+        xhttp.onload = function () {
+            const charResults = JSON.parse(this.response).Character;
+            instance.imageSrc = charResults.Portrait;
+            instance.characterJob = charResults.ActiveClassJob.UnlockedState.Name;
+        }
+        xhttp.send();
+    }
+
+    getCharacterInfo() {
+        let xhttp = new XMLHttpRequest();
+        const instance = this;
+        xhttp.open('GET', `https://xivapi.com/character/search?name=Cenne+Hyskaris&server=Lamia`);
+        xhttp.onload = function () {
+            const charResults = JSON.parse(this.response).Results[0];
+            instance.getCharacterInfoById(charResults.ID);
+        }
+        xhttp.send();
+    }
 }
